f17e1f2441de22b9c4f1161f2baae9fc17f6addb
Paulo Tolentino <paulo.tolentino@cellereit.com.br>
2020-01-22 18:13:16 -0300

    Updates

---
+++ precache-manifest.5bb63749c3e3e19a81423c8406cc2f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658bb9169f2e0d397fb3a93ec0a53eec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ce095bfb8019acaa663a",
+    "url": "/monsters-rolodex/static/css/main.cb7a0107.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce095bfb8019acaa663a",
+    "url": "/monsters-rolodex/static/js/main.f32a435c.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
